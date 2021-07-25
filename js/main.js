d2ebf39cb5cd65e6c0220b387651296b3bca4a68
SimoneBenato <sb270298@gmail.com>
2021-07-25 12:30:32 +0200

    Checco

--- js/main.js
+++ js/main.js
@@ -1,37 +1,66 @@
 // Palidroma
 
 // Chiedere all’utente di inserire una parola
-var parolaUtente = prompt("inserisci una parola");
-console.log(parolaUtente);
+// var parolaUtente = prompt("inserisci una parola");
+// console.log(parolaUtente);
 
-// Creare una funzione per capire se la parola inserita è palindroma
-function palindroma(stringa) {
-	var lowerstringa = stringa.toLowerCase();
-	var result = lowerstringa.split("").reverse().join("");
-	if (lowerstringa == result) {
-		return stringa + " " + "è una parola palindroma";
-	} else {
-		return stringa + " " + "non è una parola palindroma";
-	} 	
-}
-console.log(palindroma(parolaUtente));
-
+// // Creare una funzione per capire se la parola inserita è palindroma
+// function palindroma(stringa) {
+// 	var lowerstringa = stringa.toLowerCase();
+// 	var result = lowerstringa.split("").reverse().join("");
+// 	if (lowerstringa == result) {
+// 		return stringa + " " + "è una parola palindroma";
+// 	} else {
+// 		return stringa + " " + "non è una parola palindroma";
+// 	} 	
+// }
+// console.log(palindroma(parolaUtente));
 
 // Pari e Dispari
 
 // L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+var userNumber;
+var pariDispari 
 
+do {
+	pariDispari = prompt("Inserisci pari o dispari");
+	var lowerPariDispari = pariDispari.toLowerCase();
+} while ((lowerPariDispari != "pari") && (lowerPariDispari != "dispari"));
+console.log(lowerPariDispari);
 
-// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
-// function randomNumber(min, max) {
-// 	return Math.floor(Math.random() * (max - min)) + min;
-// }
-// cpuNumb = randomNumber(1, 6);
+do {
+	userNumber = parseInt(prompt("Inserisci un numero da 1 a 5"));
+} while ((userNumber < 1) || (userNumber > 5) || (isNaN(userNumber)));
+console.log(userNumber);
 
-// console.log(cpuNumb);
+// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione)
+function randomNumber(min, max) {
+	return Math.floor(Math.random() * (max - min)) + min;
+}
+cpuNumb = randomNumber(1, 6);
+console.log(cpuNumb);
 
-// Sommiamo i due numeri
+// // Sommiamo i due numeri
+function somma(num1, num2) {
+	var somma = num1 + num2;
+	return somma;
+}
+console.log(somma(cpuNumb, userNumber));
 
 // Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+function edoardo(num1) {
+	var alberto; 
+	if (num1 % 2 == 0)  {
+		alberto = "pari"
+	} else {
+		alberto = "dispari"
+	}
+	return alberto;
+}
 
-// Dichiariamo chi ha vinto.
+// Dichiariamo chi ha vinto
+
+
+
+
+
